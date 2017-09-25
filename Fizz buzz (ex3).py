e00a9fd41bac55553f2f9dc9b0265e571b876b89
smurfgalway <g00323639@gmit.ie>
2017-09-25 12:50:42 +0100

    fizz buzz exercise 3 complete

--- Fizz buzz (ex3).py
+++ Fizz buzz (ex3).py
@@ -1,1 +1,13 @@
 # Fizz buzz Exercise 3
+
+number =0;
+
+for number in range (1, 101):
+    s =""
+    if number % 3 == 0:
+        s += "fizz"
+    if number % 5 == 0:
+        s += "buzz"
+    if s == "":
+         s = number;
+    print (s)
