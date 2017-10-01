312fa6c84f9bd9711690918ec750f7df8887c65a
smurfgalway <g00323639@gmit.ie>
2017-10-01 19:18:24 +0100

    comments added

--- Reverse String (ex10).py
+++ Reverse String (ex10).py
@@ -1,10 +1,15 @@
 #Reverse a string Exercise 10 
-def mirrored(string):
-    
+#reversel definition with string S
+def mirrored(string):   
+    #reversed string intialised 
     mirrorSt = ''
+
+    #item that takes count of the length of the string so there starting postion is zero
     pointer = len(string)
+    #while the pointers position is greater than 0 the while continues
     while pointer > 0:
         mirrorSt += string[ pointer - 1 ]
         pointer = pointer - 1
     return mirrorSt
+#print statement that prints the set word reversed
 print(mirrored('Sham'))
