0027a421a30c55c694cd8b5b3d3fe1fac630d12c
smurfgalway <g00323639@gmit.ie>
2017-10-01 19:17:51 +0100

    comments

--- List Largest and Smallest (ex6).py
+++ List Largest and Smallest (ex6).py
@@ -1,6 +1,9 @@
 # List largest and smallest (exercise 6)
+#a basic list containing strings of vareying length
 basicList = ['Name', 'Age', 'Height', 'Weight', 'Nationality']
 
+#sorts the list by character length sorted largest string first
 basicList.sort(key=len, reverse=True)
 
-print("Items", basicList)
+#print statement that prints the list
+print("Items largest to smallest in decesnding order: ", basicList)
