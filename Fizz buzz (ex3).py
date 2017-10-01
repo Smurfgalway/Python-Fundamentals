cec97a410825d577790637441beda0f51b43b612
smurfgalway <g00323639@gmit.ie>
2017-10-01 19:17:23 +0100

    comments

--- Fizz buzz (ex3).py
+++ Fizz buzz (ex3).py
@@ -1,13 +1,17 @@
 # Fizz buzz Exercise 3
-
+#number intilised as zer0
 number =0;
-
+#for that runs within a range of 100
 for number in range (1, 101):
-    s =""
+    word =""
+    #if the number is a multiple of 3 it prints fizz
     if number % 3 == 0:
-        s += "fizz"
+        word += "fizz"
+    # if the number is a multiple of 5 it prints buzz
     if number % 5 == 0:
-        s += "buzz"
-    if s == "":
-         s = number;
-    print (s)
+        word += "buzz"
+    #if the number is not a mulitple of either it prints neither
+    if word == "":
+         word = number;
+    #if it is a multiple of both it prints fizzbuzz
+    print (word)
