7500d345b484195595f634cdca4458bc423f036e
smurfgalway <g00323639@gmit.ie>
2017-10-06 22:52:01 +0100

    factorials

--- Factorials (ex 4.py
+++ Factorials (ex 4.py
@@ -1,5 +1,12 @@
 #Factorials Exercise 4
 import math
-def Factorials(n):
-    for n < 100;
-    
+
+factorial = math.factorial(100)
+# getting the factorial of 100
+print ("The factorial of 100 = \n", factorial)
+
+#list of ints in 100
+Nums = [int(x) for x in str(factorial)]
+
+#summing all of the numbers in 100
+print("The sum of all the numbers in 100 are " , sum(Nums))
